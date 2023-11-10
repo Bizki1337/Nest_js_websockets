c5e8edf509d6bbd4fb938c1cd24fc5de3b32520d
Соловьёв Николай <bizki@MacBook-Pro-Solovev.local>
2023-11-10 14:18:01 +0300

    added event listener

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -1,6 +1,10 @@
-import { WebSocketGateway } from '@nestjs/websockets'
+import { MessageBody, SubscribeMessage, WebSocketGateway } from '@nestjs/websockets'
 
 @WebSocketGateway()
 export class MyGateway {
 
+    @SubscribeMessage('newMessage')
+	onNewMessage(@MessageBody() body: any) {
+		console.log({body})
+	}
 }
